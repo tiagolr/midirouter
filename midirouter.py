7dab614fd38c87ba04c44e38ad7f503b8ccf0b0d
grimiu <ripbizz@noemail.com>
2022-04-11 17:53:25 +0100

    .

--- midirouter.py
+++ midirouter.py
@@ -13,7 +13,9 @@ KEY_CYCLE = 46
 KEY_PREV = 58
 KEY_NEXT = 59
 KEYS_GLOBAL = [60, 61, 62, 43, 44, 42, 41, 45]
-KEYS_ARM = [64, 65, 66, 67, 68, 69, 70, 71, 48, 49, 50, 51, 52, 53, 54, 55]
+KEYS_ARM = [48, 49, 50, 51, 52, 53, 54, 55, # 0-7 channels mute buttons
+            64, 65, 66, 67, 68, 69, 70, 71, # 8-15 channels arm buttons
+            ]
 channel = 0
 isCycling = False
 
@@ -66,18 +68,27 @@ def send_channel_message(status, data1=None, data2=None, ch=channel, out=cableou
     if data1 is not None and data1 in KEYS_GLOBAL:
         msg[0] = (status & 0xF0) | (GLOBAL_CHAN & 0xF)   # overwrite with global channel
 
-    # print('sending', msg)
     out.send_message(msg)
 
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass;
-finally:
-    print('closing...')
+def shutdown():
+    global midiin, midiout, cableout
+    print('closing ports...')
     midiin[0].close_port()
     midiout[0].close_port()
     cableout[0].close_port()
     del midiin
     del midiout
+    del cableout
+
+# paint_leds()
+# import code
+# code.InteractiveConsole(locals=globals()).interact()
+# shutdown()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    shutdown()
