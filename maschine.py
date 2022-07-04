2c40895f12a0c77637e74501065c923ed7da3269
tiagolr <tiagolr88@gmail.com>
2022-07-04 14:08:01 +0100

    'there'

--- maschine.py
+++ maschine.py
@@ -1,58 +1,41 @@
 import time
 from rtmidi import midiutil
 
-midiin = midiutil.open_midiinput('maschine')
-midiout = midiutil.open_midioutput('maschine')
+midiin = midiutil.open_midiinput('Maschine')
+midiout = midiutil.open_midioutput('Maschine')
 print(midiin)
 print(midiout)
 
-CHANNEL = 0
-KEYS_GROUPS = [ 60, 61, 62, 43, 44, 42, 41, 45 ] # transport buttons
-KEYS_PADS = [
-    48, 49, 50, 51,
-    52, 53, 54, 55, # 0-7 channels mute buttons
-    64, 65, 66, 67, # 8-15 channels arm buttons
-    64, 65, 66, 67  # 8-15 channels arm buttons
-]
+GROUP = 0
+# GROUPS = [ 60, 61, 62, 43, 44, 42, 41, 45 ] # transport buttons
+# PADS = [
+#     48, 49, 50, 51,
+#     52, 53, 54, 55, # 0-7 channels mute buttons
+#     64, 65, 66, 67, # 8-15 channels arm buttons
+#     64, 65, 66, 67  # 8-15 channels arm buttons
+# ]
 
 def onmidi(event, data=None):
-    global channel, isCycling
     message, deltatime = event
     status, note, velocity = message
+    print(message, deltatime)
+    print(status, note, velocity)
     iscc = message[0] == 176
     isnote_on = velocity == 127
-    if iscc:
-        if message[1] == KEY_CYCLE:
-            isCycling = isnote_on
-            paint_leds(isnote_on)
-        elif message[1] == KEY_PREV:
-            if isnote_on:
-                channel = (channel - 1) % 16
-            paint_leds(isnote_on)
-        elif iscc and message[1] == KEY_NEXT:
-            if isnote_on:
-                channel = (channel + 1) % 16
-            paint_leds(isnote_on)
-        elif iscc and note in KEYS_ARM and isnote_on and isCycling:
-            # select channel pressing cycle+arm[1,16]
-            channel = KEYS_ARM.index(note)
-            paint_leds(True)
-        elif iscc and note in KEYS_ARM and isnote_on and isCycling:
-            # select channel pressing cycle+arm[1,16]
-            channel = KEYS_ARM.index(note)
-            paint_leds(True)
-
+    print(iscc, isnote_on)
     # pipe messages with the modified channel# virtual midicables
-    send_channel_message(status, note, velocity, channel)
+    # send_channel_message(status, note, velocity, channel) 
+
 midiin[0].set_callback(onmidi)
 
-def paint_leds(paint_channel=True):
-    for key in KEYS_ARM:
-        send_channel_message(176, key, 0, 0, midiout[0]) # clear leds
-    if paint_channel:
-        send_channel_message(176, KEYS_ARM[channel], 127, 0, midiout[0])
+# def paint_leds(paint_channel=True):
+#     for key in KEYS_ARM:
+#         send_channel_message(176, key, 0, 0, midiout[0]) # clear leds
+#     if paint_channel:
+#         send_channel_message(176, KEYS_ARM[channel], 127, 0, midiout[0])
 
-def send_channel_message(status, data1=None, data2=None, ch=channel, out=cableout[0]):
+# def send_channel_message(status, data1=None, data2=None, ch=GROUP, out=cableout[0]):
+def send_channel_message(status, data1=None, data2=None, ch=GROUP):
     """Send a MIDI channel mode message."""
     msg = [(status & 0xF0) | (ch & 0xF)]
 
@@ -62,9 +45,12 @@ def send_channel_message(status, data1=None, data2=None, ch=channel, out=cableou
             msg.append(data2 & 0x7F)
 
     if data1 is not None and data1 in KEYS_GLOBAL:
-        msg[0] = (status & 0xF0) | (GLOBAL_CHAN & 0xF)   # overwrite with global channel
+        # overwrite with global channel
+        msg[0] = (status & 0xF0) | (GLOBAL_CHAN & 0xF)
 
-    out.send_message(msg)
+        print_message(msg)
+    # out.send_message(msg)
+
 
 def shutdown():
     global midiin, midiout
@@ -73,6 +59,7 @@ def shutdown():
     midiout[0].close_port()
     del midiin
     del midiout
+
 
 try:
     while True:
